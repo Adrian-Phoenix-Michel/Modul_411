8a08c809d3d1aa2d204620d4571e790aff3ef6ae
adrian.michel <adrian.p.michel@hotmail.com>
2019-10-18 11:32:05 +0200

    Version 0.1 master

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,24 +1,40 @@
+import java.sql.SQLOutput;
+
 public class InsertionSort {
 
     void insertionSort(int A[]) {
-        int n = A.length - 1;
+        int n = A.length;
         int i = 0;
         int j = 0;
         int value = A[i];
 
-        for (i = 1; i > n; i++) {
+        for (i = 1; i < n; ++i) {
             value = A[i];
-            j = i;
-            while (j >= 0 && A[j - 1] > value) {
-                A[j] = A[j - 1];
+            j = i - 1;
+            while (j >= 0 && A[j] > value) {
+                A[j + 1] = A[j];
                 j = j - 1;
             }
-            A[j] = value;
+            A[j + 1] = value;
 
         }
     }
 
-    public static void main(String[] args) {
+    private static void printArray(int A[])
+    {
+        int n = A.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(A[i] + " ");
 
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int A[] = { 12, 11, 13, 5, 6 };
+
+        InsertionSort ob = new InsertionSort();
+        ob.insertionSort(A);
+
+        printArray(A);
     }
 }
