91fc86782d974cf577a29f115bb772043189eead
adrian.michel <adrian.p.michel@hotmail.com>
2019-10-18 11:52:44 +0200

    Version 0.2 developer

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,24 +1,26 @@
 public class InsertionSort {
 
-    void insertionSort(int A[]) {
-        int n = A.length - 1;
+    static void insertionSort(int[] A) {
+        int n = A.length;
         int i = 0;
         int j = 0;
         int value = A[i];
 
-        for (i = 1; i > n; i++) {
+        for (i = 1; i < n; ++i) {
             value = A[i];
-            j = i;
-            while (j >= 0 && A[j - 1] > value) {
-                A[j] = A[j - 1];
+            j = i - 1;
+            while (j >= 0 && A[j] > value) {
+                A[j + 1] = A[j];
                 j = j - 1;
             }
-            A[j] = value;
+            A[j + 1] = value;
 
         }
     }
 
     public static void main(String[] args) {
-
+        int[] A = {0, 9, 4, 6, 2, 8, 5, 1, 7, 3};
+        insertionSort(A);
+        for (int value : A) System.out.print(value + " ");
     }
 }
